2998c01e9f7fa46aa8a5d9984574348341eae371
Chris OSullivan <CoSully@gmail.com>
2019-06-20 18:39:40 +0100

    Updates

---
+++ precache-manifest.065e9c9eec3cae11daf15df29fae61a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04da997ec7b3fdfc361c740e16d5be5",
+    "url": "/react-dice/index.html"
+  },
+  {
+    "revision": "f7765bfee0140b62b0c1",
+    "url": "/react-dice/static/css/main.9e391980.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/react-dice/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "f7765bfee0140b62b0c1",
+    "url": "/react-dice/static/js/main.05fe8ff9.chunk.js"
+  },
+  {
+    "revision": "b53b16400a8a29e4d3fd",
+    "url": "/react-dice/static/js/runtime~main.9b5673c1.js"
+  }
+]);
